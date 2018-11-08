847630226b06afe9ee809c44aa972c9cf9e97c2a
enrique101 <38600968+enrique101@users.noreply.github.com>
2018-11-07 23:25:09 -0600

    contact form added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 require('dotenv').config({ path: '.env' });
 const express = require('express');
 const socketIO = require('socket.io');
+const  bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { sendEmail } = require('./utils/email');
 const { message } = require('./utils/watson');
 const { isValidString } = require('./utils/validators');
 const contextList = require('./utils/context');
@@ -12,16 +15,51 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(helmet());
 app.use(cors());
+
+const router = express.Router();
+app.use('/api', router);
+
+const corsOpt = {
+    origin: process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200
+  }
+
+router.route('/contact', cors(corsOpt))
+    .post((req,res) => {
+        "use strict"
+        const emailRegex = new RegExp('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+        let { name, email, text } = req.body;
+        let result = {};
+        name = name.trim().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        email = email.trim().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        text = text.trim().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        if(email !== '' && emailRegex.test(email) && text !== '' && name !== ''){
+            result = sendEmail(name, email, text).catch(e => {result = e;});
+        }
+        else{
+            result = {
+                error: "Invalid data",
+            }
+        }
+        res.json(result);
+    });
+
 
 const server = http.createServer(app);
 
 const io = socketIO(server);
+io.origins([process.env.FRONTEND_URL]);
 
 io.on('connection', socket =>{
+    "use strict"
     socket.on('createMessage', async (data, fn) =>{
         if(isValidString(data.text)){
-            const res = await message(socket.id,data.text);
+            const res = await message(socket.id,data.text.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
             io.to(socket.id).emit('newMessage', res);
             fn();
         }
---
+++ server/utils/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const sendEmail = async (name,email, text) =>{
+  "use strict"
+    return await new Promise((resolve,reject)=>{
+      transport.sendMail({
+            from: 'enrique.raspberry@gmail.com',
+            to: 'enrique.acuna@gmail.com',
+            subject: `Mensaje de ${name}`,
+            html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>${text.replace(/[\r\n]/g, "<br />")}<p/>`,
+          }, (error,info) => {
+            if(error) reject({ error });
+            resolve(info);
+          });
+
+    });
+};
+
+module.exports.sendEmail = sendEmail;
