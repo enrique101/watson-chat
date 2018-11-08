4bc8bd832d9ffa8712c429f42a5016082728a8e0
enrique101 <38600968+enrique101@users.noreply.github.com>
2018-11-08 00:15:23 -0600

    cors test

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(helmet());
 
 app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
    });
